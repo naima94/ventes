04d93af29da78a61a4811e3368ebf27cfe5a0e31
naima94 <naimatou.adambi@gmail.com>
2021-08-26 21:14:14 -0800

    Popup

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ if (hamburger) {
         navList.classList.toggle("open");
     })
 }
+
+// Popup
+const popup = document.querySelector(".popup");
+const closePopup = document.querySelector("popup-close");
+
+if (popup) {
+    closePopup.addEventListener("click", () => {
+        popup.classList.add("hide-popup");
+    })
+
+    window.addEventListener("load", () => {
+        setTimeout(() => {
+            popup.classList.remove("hide-popup");
+        }, 1000);
+    });
+}
